17fcab740b60003d2c5ef8a1b77c350f5c342b86
ZJ0724 <53032500+ZJ0724@users.noreply.github.com>
2021-01-19 16:06:49 +0800

    加注释

--- src/main/java/com/sunny/BgcDemoApplication.java
+++ src/main/java/com/sunny/BgcDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BgcDemoApplication {
 
     public static void main(String[] args) {
+        // 这是周君加的注释
         SpringApplication.run(BgcDemoApplication.class, args);
     }
 
