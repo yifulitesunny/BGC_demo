605182cce230b87b9a87115df669ef91fbb762d0
ZJ0724 <53032500+ZJ0724@users.noreply.github.com>
2021-01-19 16:10:16 +0800

    Update BgcDemoApplication.java

--- src/main/java/com/sunny/BgcDemoApplication.java
+++ src/main/java/com/sunny/BgcDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BgcDemoApplication {
 
     public static void main(String[] args) {
+        // 测试注释
         SpringApplication.run(BgcDemoApplication.class, args);
     }
 
