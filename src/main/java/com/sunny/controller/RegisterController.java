2a96272ac08260a35108aa050abda71ed885cdf4
yifulitesunny <1164806928@qq.com>
2021-01-19 15:59:00 +0800

    删除注释代码，尝试提交代码功能

--- src/main/java/com/sunny/controller/RegisterController.java
+++ src/main/java/com/sunny/controller/RegisterController.java
@@ -30,40 +30,4 @@ public class RegisterController {
             return ResponseEntity.normalReturn(e.getMessage(), 101, null);
         }
     }
-
-//    @GetMapping("test")
-//    public ResponseEntity test() {
-//        // 打印线程名
-//        System.out.println(Thread.currentThread().getName());
-//        // 模拟服务器响应慢，假如要响应10秒
-//        try {
-//            Thread.sleep(10000);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return ResponseEntity.normalReturn("", 200, "");
-//    }
-//
-//    @GetMapping("test2")
-//    public ResponseEntity test2() {
-//        // 打印线程名
-//        System.out.println(Thread.currentThread().getName());
-//        // 模拟服务器响应慢，假如要响应5秒
-//        try {
-//            Thread.sleep(5000);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return ResponseEntity.normalReturn("", 200, "");
-//    }
-//
-//    @GetMapping("test3")
-//    public ResponseEntity test3() {
-//        // 如果没有异常处理，这里代码在运行时会直接报错抛出，导致程序异常
-//        try {
-//            throw new ParamErrorException();
-//        } catch (Exception e) {
-//            throw new ParamErrorException();
-//        }
-//    }
 }
